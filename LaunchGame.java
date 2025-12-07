a61170365b39640b801dcbbd4aecae02e5e1bd1f
RohanBhanot06 <rohan.bhanot06@gmail.com>
2025-12-06 23:48:35 -0500

    Add documentation comments to LaunchGame class
    Added class documentation for LaunchGame.

--- LaunchGame.java
+++ LaunchGame.java
@@ -1,8 +1,14 @@
-package battleship;
+/* Name: Rohan Bhanot 
+ * Starting Date: Friday, April 26, 2024
+ * Ending Date: Wednesday, June 12, 2024
+ * Teacher: Elena Kapustina
+ * Course Code: ICS4U1-2
+ * Program Name: LaunchGame
+ * Description: Creating the LaunchGame class for my Battleship Culminating Project.
+ */
 
-public class LaunchGame{
+public class LaunchGame {
 	public static void main(String[] args) {
 		new GameLogic().setUpWindow();
 	}
 }
-
