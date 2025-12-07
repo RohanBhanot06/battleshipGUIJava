225cd59dd6efb39df49b6d4b87c156b7f8dbcd54
RohanBhanot06 <rohan.bhanot06@gmail.com>
2025-12-06 23:49:32 -0500

    Enhance Ship class with detailed comments
    Added documentation comments to constructors and methods, clarifying purpose and parameters.

--- Ship.java
+++ Ship.java
@@ -1,51 +1,79 @@
-package battleship;
+/* Name: Rohan Bhanot 
+ * Starting Date: Friday, April 26, 2024
+ * Ending Date: Wednesday, June 12, 2024
+ * Teacher: Elena Kapustina
+ * Course Code: ICS4U1-2
+ * Program Name: Ship
+ * Description: Creating the Ship class for my Battleship Culminating Project.
+ */
 
 import java.awt.Point;
 import java.util.ArrayList;
 
 public class Ship {
 
-	private ShipPiece[] pieces;
-	private Point startingPosition;
+	private ShipPiece[] pieces; // Array of ShipPiece objects representing the pieces of the ship
+	private Point startingPosition; // Point representing the starting position of the ship off the grid
 
 	/*
-	 * Constructor. Sets pieces equal to the list parameter
+	 * Purpose: Constructor. Initializes the Ship with an array of ShipPiece objects.
+	 * PRE: ShipPiece[] list
+	 * POST: None
 	 */
 	Ship(ShipPiece[] list) {
-		pieces = list;
-		startingPosition = new Point(0,0);
-	}
-	
-	Ship(ArrayList<ShipPiece> list){
-		pieces = list.toArray(new ShipPiece[0]);
-		startingPosition = new Point(0,0);
+		pieces = list; // Assign the array of ShipPiece objects to pieces
+		startingPosition = new Point(0, 0); // Initialize startingPosition to (0, 0)
 	}
 
 	/*
-	 * If all the ship pieces are dead, it returns that the ship is dead
+	 * Purpose: Constructor. Initializes the Ship with an ArrayList of ShipPiece objects.
+	 * PRE: ArrayList<ShipPiece> list
+	 * POST: None
+	 */
+	Ship(ArrayList<ShipPiece> list) {
+		pieces = list.toArray(new ShipPiece[0]); // Convert ArrayList to array and assign to pieces
+		startingPosition = new Point(0, 0); // Initialize startingPosition to (0, 0)
+	}
+
+	/*
+	 * Purpose: Checks if all ship pieces are destroyed, indicating the ship is dead.
+	 * PRE: None
+	 * POST: boolean (true if the ship is dead, false otherwise)
 	 */
 	public boolean checkIfDead() {
-		boolean isDead = true;
-		for (int i = 0; i < pieces.length; i++) {
-			if (!pieces[i].isDestroy()) {
-				isDead = false;
+		boolean isDead = true; // Initialize isDead to true
+		for (int i = 0; i < pieces.length; i++) { // Iterate through all pieces of the ship
+			if (!pieces[i].isDestroy()) { // If any piece is not destroyed
+				isDead = false; // Set isDead to false
 			}
 		}
-		return isDead;
+		return isDead; // Return the status of the ship
 	}
 
 	/*
-	 * returns the array of ship pieces
+	 * Purpose: Returns the array of ShipPiece objects.
+	 * PRE: None
+	 * POST: pieces
 	 */
 	public ShipPiece[] getShipPieces() {
-		return pieces;
+		return pieces; // Return the array of ShipPiece objects
 	}
-	
-	public void setStartingOffGridPosition(Point sp){
-		startingPosition = sp;
+
+	/*
+	 * Purpose: Sets the starting off-grid position of the ship.
+	 * PRE: Point sp
+	 * POST: None
+	 */
+	public void setStartingOffGridPosition(Point sp) {
+		startingPosition = sp; // Assign the provided point to startingPosition
 	}
-	
-	public Point getStartingOffGridPosition(){
-		return startingPosition;
+
+	/*
+	 * Purpose: Returns the starting off-grid position of the ship.
+	 * PRE: None
+	 * POST: startingPosition
+	 */
+	public Point getStartingOffGridPosition() {
+		return startingPosition; // Return the startingPosition
 	}
 }
